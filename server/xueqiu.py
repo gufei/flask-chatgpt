be927664dd849406216286702ce424ef908ce7c1
rowan.wu <>
2023-04-23 11:12:07 +0800

    提前加载索引

--- server/xueqiu.py
+++ server/xueqiu.py
@@ -7,6 +7,8 @@ from cmd.index import xueqiu_index_path, service_context
 
 from llama_index.prompts.prompts import QuestionAnswerPrompt
 
+xueqiuIndex = GPTSimpleVectorIndex.load_from_disk(xueqiu_index_path, service_context=service_context)
+
 QUESTION_ANSWER_PROMPT_TMPL_CN = (
     "你是一个经济学专家，对二级市场投资非常精通，以下是相关财经内容： \n"
     "---------------------\n"
@@ -21,8 +23,8 @@ class XueQiu(Resource):
         body = request.get_json()
         app.logger.info(body['prompt'])
         prompt = body['prompt']
-        index = GPTSimpleVectorIndex.load_from_disk(xueqiu_index_path, service_context=service_context)
-        answer = index.query(prompt, service_context=service_context,
-                             text_qa_template=QuestionAnswerPrompt(QUESTION_ANSWER_PROMPT_TMPL_CN))
+
+        answer = xueqiuIndex.query(prompt, service_context=service_context,
+                                   text_qa_template=QuestionAnswerPrompt(QUESTION_ANSWER_PROMPT_TMPL_CN))
 
         return {'code': 200, 'msg': 'ok', 'data': answer.response}
